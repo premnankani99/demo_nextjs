6e7c528e51183a988f71529bff28961b0db936f6
premnankani99 <premnankani99@gmail.com>
2026-05-21 21:31:03 +0530

    Fixed blank page layout issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <ScrollObserver />
+        {children}
+      </body>
     </html>
   );
 }
